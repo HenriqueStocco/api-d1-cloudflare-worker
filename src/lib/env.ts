b7aad6dcb9c8e6dce9c5a5a9b97a6ac51e388a4c
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-10-18 12:26:24 -0300

    refactor: remove lib/env.ts parse

--- src/lib/env.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-const envSchema = z.object({
-  CLOUDFLARE_ACCOUNT_ID: z.string(),
-  CLOUDFLARE_DATABASE_ID: z.string(),
-  CLOUDFLARE_D1_TOKEN: z.string(),
-})
-
-export const env = envSchema.parse(process.env)
