45ef8d777e4df102c025800fb0bb581f080158e4
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-10-22 13:51:06 -0300

    feat: Add headers & routes & bindings

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,41 @@
 import { Hono } from 'hono'
-import { notes } from './router/notes'
-import type { Client } from 'pg'
+import { logger } from 'hono/logger'
+import { note } from './router/notes'
+import { user } from './router/users'
 
 export type Bindings = {
-  DATABASE_URL: Client
+  DATABASE_URL: string
 }
 
 const app = new Hono<{ Bindings: Bindings }>().basePath('/api')
 
-app.route('/notes', notes)
+app.use(logger())
 
-export default{
+function setCorsHeaders(res: Response) {
+  res.headers.set('Access-Control-Allow-Origin', '*')
+  res.headers.set('Access-Control-Request-Method', '*')
+  res.headers.set(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH',
+  )
+  res.headers.set('Access-Control-Allow-Headers', 'Authorization, Content-Type')
+  res.headers.set('Access-Control-Max-Age', '86400')
+  res.headers.set('Content-Type', 'application/json')
+}
+
+app.route('/notes', note)
+app.route('/users', user)
+
+export default {
   async fetch(request: Request, env: Bindings, ctx: ExecutionContext) {
+    if (request.method === 'OPTIONS') {
+      const response = new Response(null, {
+        status: 204,
+      })
+      setCorsHeaders(response)
+      return response
+    }
+
     return app.fetch(request, env, ctx)
-  }
+  },
 }
