fd4369774ecfea7f4caa5cbcdf55228f2a152c47
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-10-22 13:49:35 -0300

    feat: add user and session table for to use sessions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,20 @@
-import { type InferSelectModel, relations } from 'drizzle-orm'
-import { integer, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import { pgTable, serial, timestamp, uuid, varchar } from 'drizzle-orm/pg-core'
 
-export const user = pgTable('user', {
-  id: uuid('id').primaryKey().notNull().unique(),
+export const userSession = pgTable('session', {
+  id: varchar('id').primaryKey().notNull(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id),
+
+  expiresAt: timestamp('expires_at', {
+    mode: 'date',
+    withTimezone: false,
+  }).notNull(),
+})
+
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey().notNull().defaultRandom(),
   name: varchar('name').notNull(),
   email: varchar('email').notNull(),
   password: varchar('password').notNull(),
@@ -10,46 +22,40 @@ export const user = pgTable('user', {
   createdAt: timestamp('created_at', {
     withTimezone: false,
     mode: 'date',
-  }).notNull(),
+  })
+    .notNull()
+    .defaultNow(),
 })
 
-export const userRelations = relations(user, ({ many }) => ({
-  notes: many(note),
+export const userRelations = relations(users, ({ many }) => ({
+  notes: many(notes),
 }))
 
-export const userSession = pgTable('session', {
-  id: uuid('id')
-    .primaryKey()
-    .notNull()
-    .unique(),
-  userId: uuid('user_id')
-    .notNull()
-    .references(() => user.id),
-
-  expiresAt: timestamp('expires_at', {
-    mode: 'date',
-    withTimezone: false
-  }).notNull(),
-})
-
-export const note = pgTable('note', {
-  id: integer('id').primaryKey().notNull(),
-  title: varchar('title', {length: 100}).notNull(),
+export const notes = pgTable('notes', {
+  id: serial('id').primaryKey().notNull(),
+  title: varchar('title', { length: 100 }).notNull(),
   description: varchar('description').notNull(),
   userId: uuid('user_id')
     .notNull()
-    .references(() => user.id),
+    .references(() => users.id),
 
-  createdAt: timestamp('created_at', { mode: 'date', withTimezone: false }).notNull(),
-  updatedAt: timestamp('updated_at', { mode: 'date', withTimezone: false }).notNull(),
+  createdAt: timestamp('created_at', {
+    mode: 'date',
+    withTimezone: false,
+  })
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp('updated_at', {
+    mode: 'date',
+    withTimezone: false,
+  })
+    .notNull()
+    .defaultNow(),
 })
 
-export const noteRelations = relations(note, ({ one }) => ({
-  user: one(user, {
-    fields: [note.userId],
-    references: [user.id],
+export const noteRelations = relations(notes, ({ one }) => ({
+  user: one(users, {
+    fields: [notes.userId],
+    references: [users.id],
   }),
 }))
-
-export type User = InferSelectModel<typeof user>
-export type Session = InferSelectModel<typeof userSession>
