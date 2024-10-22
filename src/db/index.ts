0919a47342f8e81757e0f66670454e3489abacc5
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-10-22 13:50:30 -0300

    feat: create a connection with Neon Serverless Postgres database

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,11 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { neon } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema'
-import type { Client } from 'pg'
 
-export const pgDrizzle = (client: Client) => {
-  return drizzle(client, { schema, logger: true })
+export const pgDrizzle = (client: string) => {
+  const neonClient = neon(client)
+  return drizzle(neonClient, { schema, logger: true })
 }
 
 export { eq, and, asc, sql, lte } from 'drizzle-orm'
 export { schema }
-export type { Client }
